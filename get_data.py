9bba59568a6624fc4052861d20a4384eeaaac80d
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-08-18 05:08:27 +0500

    data to json

--- get_data.py
+++ get_data.py
@@ -1,5 +1,5 @@
 import json
-
+from pprint import pprint
 def get_data(filename:str) -> dict:
     """
     You are given a filename. Read the JSON data from the file and return the dictionary.
@@ -9,3 +9,8 @@ def get_data(filename:str) -> dict:
     Returns:
         dict: JSON data
     """
+    data=json.loads(d)
+    return data
+f=open('randomuser_data.json','r')
+d=f.read()
+pprint(get_data(f))
