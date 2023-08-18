0db5cad40b5dbafe1978d33eaef97df97387b5e1
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-08-18 05:48:41 +0500

    email print

--- get_email.py
+++ get_email.py
@@ -1,5 +1,5 @@
-import get_data
-
+import json
+from pprint import pprint
 def get_email(data:dict) -> list:
     """
     Take the email of the users and return the list.
@@ -9,3 +9,11 @@ def get_email(data:dict) -> list:
     Returns:
         list: users email
     """
+    results=data.get('results')
+    l=[]
+    for i in results:
+        l.append(i['email'])
+    return l
+f=open('randomuser_data.json','r')
+data=json.loads(f.read())
+pprint(get_email(data))   
